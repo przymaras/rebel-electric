a5b8b400f7b295f3df81717591311b679ec23aba
przymaras <przymaras@gmail.com>
2022-01-09 13:11:13 +0100

    wip adding form

--- components/hangar/AddEbikeForm.js
+++ components/hangar/AddEbikeForm.js
@@ -1,90 +1,16 @@
 import { Formik, Field, Form, ErrorMessage, useField } from "formik";
 import * as Yup from "yup";
+import {
+  TextInput,
+  TextArea,
+  Select,
+  RadioInput,
+  Fieldset,
+  SubmitButton,
+} from "../layout/formInputs";
 
-import AddEbikeDataGroup from "./AddEbikeDataGroup";
+import AddVehicleDataGroup from "./AddVehicleDataGroup";
 import styles from "./AddEbikeForm.module.css";
-
-function HeadingTextInput({ label, description, ...props }) {
-  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
-  // which we can spread on <input> and alse replace ErrorMessage entirely.
-  const [field, meta] = useField(props);
-  return (
-    <div>
-      <label htmlFor={props.id || props.name}>
-        <h2 className="rebel-font">{label}</h2>
-      </label>
-      <input className="text-input" {...field} {...props} />
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-      {description ? <p>{description}</p> : null}
-    </div>
-  );
-}
-
-function TextInput({ label, description, ...props }) {
-  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
-  // which we can spread on <input> and alse replace ErrorMessage entirely.
-  const [field, meta] = useField(props);
-  return (
-    <div>
-      <label htmlFor={props.id || props.name}>{label}</label>
-      <input className="text-input" {...field} {...props} />
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-      {description ? <p>{description}</p> : null}
-    </div>
-  );
-}
-
-function HeadingTextArea({ label, description, ...props }) {
-  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
-  // which we can spread on <input> and alse replace ErrorMessage entirely.
-  const [field, meta] = useField(props);
-  return (
-    <div>
-      <label htmlFor={props.id || props.name}>
-        <h2 className="rebel-font">{label}</h2>
-      </label>
-      <textarea className="text-area" {...field} {...props} />
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-      {description ? <p>{description}</p> : null}
-    </div>
-  );
-}
-
-const Select = ({ label, ...props }) => {
-  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
-  // which we can spread on <input> and alse replace ErrorMessage entirely.
-  const [field, meta] = useField(props);
-  return (
-    <div>
-      <label htmlFor={props.id || props.name}>{label}</label>
-      <select {...field} {...props} />
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-    </div>
-  );
-};
-
-function RadioInput({ label, ...props }) {
-  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
-  // which we can spread on <input> and alse replace ErrorMessage entirely.
-  const [field, meta] = useField(props);
-  return (
-    <>
-      <input className="radio-input" {...field} {...props} />
-      <label htmlFor={props.id || props.name}>{label}</label>
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-    </>
-  );
-}
 
 function AddEbikeForm(props) {
   return (
@@ -93,14 +19,35 @@ function AddEbikeForm(props) {
         projectName: "",
         video: "",
         description: "",
+        manufacturer: "",
+        model: "",
         year: "",
+        wheelSize: "",
+        brakes: "",
+        mass: "",
+        vmax: "",
+        range: "",
+        controller: "",
+        ctrlVoltage: "",
+        ctrlCurrent: "",
+        motor: "",
+        motorModel: "",
+        batteryType: "",
+        cellsType: "",
+        batVoltage: "",
+        batSeries: "",
+        batParallels: "",
+        cellsManuf: "",
+        cellsModel: "",
+        capacityWh: "",
+        capacityAh: "",
       }}
       validationSchema={Yup.object({
         projectName: Yup.string()
           .max(15, "Must be 15 characters or less")
           .required("Required"),
-        video: Yup.string().url("Must be valid URL").required("Required"),
-        description: Yup.string().required("Required"),
+        video: Yup.string().url("Must be valid URL"),
+        description: Yup.string(),
         year: Yup.number()
           .typeError("Must be a year")
           .min(1900, "Must be a year")
@@ -113,116 +60,267 @@ function AddEbikeForm(props) {
         }, 400);
       }}
     >
-      <div className={styles.container}>
-        <Form>
-          <HeadingTextInput
-            label="Nazwa Projektu:"
-            name="projectName"
-            type="text"
-            placeholder=""
-            description="(będzie wyświetlana nad galerią)"
-          />
-          <div>
-            <h2 className="rebel-font">Dodaj zdjęcia:</h2>
-            <div>
-              <div>Foto 1</div>
-              <div>Foto 2</div>
-            </div>
-          </div>
-          <HeadingTextInput
-            label="Dodaj prezentację video!"
-            name="video"
-            type="text"
-            placeholder=""
-            description=""
-          />
+      {(formik) => {
+        return (
+          <div className={styles.container}>
+            <Form>
+              <TextInput
+                label="Nazwa Projektu:"
+                name="projectName"
+                type="text"
+                placeholder=""
+                description="(będzie wyświetlana nad galerią)"
+                rebelHeading={true}
+              />
+              <div>
+                <h2 className="rebel-font">Dodaj zdjęcia:</h2>
+                <div>
+                  <div>Foto 1</div>
+                  <div>Foto 2</div>
+                </div>
+              </div>
+              <TextInput
+                label="Dodaj prezentację video!"
+                name="video"
+                type="text"
+                placeholder=""
+                description=""
+                rebelHeading={true}
+              />
 
-          <HeadingTextArea
-            label="Dodaj opis:"
-            name="description"
-            type="text-area"
-            placeholder=""
-            description="Dodaj opis: Napisz kilka słów o swoim pojeździe - pochwal się jego
+              <TextArea
+                label="Dodaj opis:"
+                name="description"
+                type="text-area"
+                placeholder=""
+                rebelHeading={true}
+                description="Napisz kilka słów o swoim pojeździe - pochwal się jego
             historią, osiągami i ile lat go użytkujesz. Jak zaczęła się Twoja
             historia z elektrykami? Użytkownicy Rebel-electric chętnie
             dowiedzą się więcej informacji o Tobie i Twoim e-bike'u!"
-          />
-
-          <div>
-            <AddEbikeDataGroup type="baza">
-              <Select label="Marka" name="manufacturer">
-                <option value="">Marka</option>
-                <option value="manufacturer1">Marka1</option>
-                <option value="manufacturer2">Marka2</option>
-              </Select>
-
-              <Select label="Model" name="model">
-                <option value="">Model</option>
-                <option value="model1">Model1</option>
-                <option value="model2">Model2</option>
-              </Select>
-
-              <TextInput
-                label="Rok produkcji bazy"
-                name="year"
-                type="text"
-                placeholder=""
-                description=""
               />
 
-              <div>
-                <fieldset>
-                  <legend>Rozmiar kół</legend>
+              <p className={styles.info}>
+                Poniższe pola są opcjonalne, jednak bardzo ważne dla lepszego
+                poznania Twojej maszyny! Nie rezygnuj z nich lub uzupelnij je w
+                późniejszym czasie! Możesz uzupełnić tylko niektóre z nich, a
+                pozostałe zostawić puste ...
+              </p>
+              <div className={styles.data}>
+                <AddVehicleDataGroup style="base" name="baza">
+                  <Select label="Marka" name="manufacturer">
+                    <option value="">Marka</option>
+                    <option value="manufacturer1">Marka1</option>
+                    <option value="manufacturer2">Marka2</option>
+                  </Select>
 
-                  <RadioInput
-                    label="20 cali"
-                    type="radio"
-                    name="wheelSize"
-                    value="20"
+                  <Select label="Model" name="model">
+                    <option value="">Model</option>
+                    <option value="model1">Model1</option>
+                    <option value="model2">Model2</option>
+                  </Select>
+
+                  <TextInput
+                    label="Rok produkcji bazy"
+                    name="year"
+                    type="text"
+                    placeholder=""
+                    description=""
                   />
-                  <RadioInput
-                    label="24 cali"
-                    type="radio"
-                    name="wheelSize"
-                    value="24"
+
+                  <Fieldset legend="Rozmiar kół">
+                    <RadioInput
+                      label="20''"
+                      type="radio"
+                      name="wheelSize"
+                      value="20"
+                    />
+                    <RadioInput
+                      label="24''"
+                      type="radio"
+                      name="wheelSize"
+                      value="24"
+                    />
+                    <RadioInput
+                      label="26''"
+                      type="radio"
+                      name="wheelSize"
+                      value="26"
+                    />
+                    <RadioInput
+                      label="27,5''"
+                      type="radio"
+                      name="wheelSize"
+                      value="27.5"
+                    />
+                    <RadioInput
+                      label="28/29''"
+                      type="radio"
+                      name="wheelSize"
+                      value="28"
+                    />
+                    <RadioInput
+                      label="18'' moto"
+                      type="radio"
+                      name="wheelSize"
+                      value="18"
+                    />
+                    <RadioInput
+                      label="19'' moto"
+                      type="radio"
+                      name="wheelSize"
+                      value="19"
+                    />
+                    <RadioInput
+                      label="Inne"
+                      type="radio"
+                      name="wheelSize"
+                      value="other"
+                    />
+                  </Fieldset>
+
+                  <Select label="Rodzaj hamulców" name="brakes">
+                    <option value="">Rodzaj hamulców</option>
+                    <option value="brakes1">Rodzaj hamulców1</option>
+                    <option value="brakes2">Rodzaj hamulców2</option>
+                  </Select>
+
+                  <TextInput
+                    label="Masa [kg]"
+                    name="mass"
+                    type="text"
+                    placeholder=""
+                    description=""
                   />
-                </fieldset>
+
+                  <TextInput
+                    label="Prędkość maksymalna [km/h]"
+                    name="vmax"
+                    type="text"
+                    placeholder=""
+                    description=""
+                  />
+
+                  <TextInput
+                    label="Zasięg roweru [km]"
+                    name="range"
+                    type="text"
+                    placeholder=""
+                    description=""
+                  />
+                </AddVehicleDataGroup>
+                <AddVehicleDataGroup style="electrical" name="elektryka">
+                  <Select label="Sterownik" name="controller">
+                    <option value="">Wybierz sterownik</option>
+                    <option value="controller1">Sterownik1</option>
+                    <option value="controller2">Sterownik2</option>
+                  </Select>
+
+                  <TextInput
+                    label="Napięcie nominalne sterownika [V]"
+                    name="ctrlVoltage"
+                    type="text"
+                    placeholder=""
+                    description=""
+                  />
+
+                  <TextInput
+                    label="Prąd maksymalny pobierany z baterii [A]"
+                    name="ctrlCurrent"
+                    type="text"
+                    placeholder=""
+                    description=""
+                  />
+
+                  <Select label="Marka silnika" name="motor">
+                    <option value="">Wybierz markę</option>
+                    <option value="motor1">Silnik1</option>
+                    <option value="motor2">Silnik2</option>
+                  </Select>
+
+                  <Select label="Model silnika" name="motorModel">
+                    <option value="">Wybierz model</option>
+                    <option value="motorModel1">Model1</option>
+                    <option value="motorModel2">Model2</option>
+                  </Select>
+                </AddVehicleDataGroup>
+
+                <AddVehicleDataGroup style="battery" name="bateria">
+                  <Select label="Sposób montażu baterii" name="batteryType">
+                    <option value="">Wybierz typ</option>
+                    <option value="batteryType1">Typ1</option>
+                    <option value="batteryType2">Typ2</option>
+                  </Select>
+
+                  <Select label="Typ ogniw" name="cellsType">
+                    <option value="">Wybierz typ</option>
+                    <option value="cellsType1">Typ1</option>
+                    <option value="cellsType2">Typ2</option>
+                  </Select>
+
+                  <TextInput
+                    label="Napięcie nominalne [V]"
+                    name="batVoltage"
+                    type="text"
+                    placeholder=""
+                    description=""
+                  />
+
+                  <TextInput
+                    label="Il. połączeń szeregowych / S"
+                    name="batSeries"
+                    type="text"
+                    placeholder=""
+                    description=""
+                  />
+
+                  <TextInput
+                    label="Il. połączeń równoległych / P"
+                    name="batParallels"
+                    type="text"
+                    placeholder=""
+                    description=""
+                  />
+
+                  <Select label="Marka ogniw" name="cellsManuf">
+                    <option value="">Wybierz markę</option>
+                    <option value="cellsManuf1">Marka1</option>
+                    <option value="cellsManuf2">Marka2</option>
+                  </Select>
+
+                  <Select label="Model ogniw" name="cellsModel">
+                    <option value="">Wybierz model</option>
+                    <option value="cellsModel1">Model1</option>
+                    <option value="cellsModel2">Model2</option>
+                  </Select>
+
+                  <TextInput
+                    label="Pojemność w watogodzinach [Wh]"
+                    name="capacityWh"
+                    type="text"
+                    placeholder=""
+                    description=""
+                  />
+
+                  <TextInput
+                    label="Pojemność w amperogodzinach [Ah]"
+                    name="capacityAh"
+                    type="text"
+                    placeholder=""
+                    description=""
+                  />
+                </AddVehicleDataGroup>
               </div>
 
-              <Select label="Rodzaj hamulców" name="brakes">
-                <option value="">Rodzaj hamulców</option>
-                <option value="brakes1">Rodzaj hamulców1</option>
-                <option value="brakes2">Rodzaj hamulców2</option>
-              </Select>
-
-              <TextInput
-                label="Masa [kg]"
-                name="mass"
-                type="text"
-                placeholder=""
-                description=""
+              <SubmitButton
+                formik={formik}
+                text="Dodaj"
+                errorMsg=" Uzupełnij wszystkie wymagane pola!"
               />
-
-              <TextInput
-                label="Prędkość maksymalna [km/h]"
-                name="vmax"
-                type="text"
-                placeholder=""
-                description=""
-              />
-
-              <TextInput
-                label="Zasięg roweru [km]"
-                name="range"
-                type="text"
-                placeholder=""
-                description=""
-              />
-            </AddEbikeDataGroup>
+            </Form>
           </div>
-          <button type="submit">Dodaj</button>
-        </Form>
-      </div>
+        );
+      }}
     </Formik>
   );
 }
