ea9607dd952e1fd0b870d36b7cd1f14aa9498110
przymaras <przymaras@gmail.com>
2022-01-18 18:54:19 +0100

    Use data from database

--- components/hangar/VehicleBox.js
+++ components/hangar/VehicleBox.js
@@ -5,33 +5,36 @@ import Image from "next/image";
 import DataBar from "./DataBar";
 
 import styles from "./VehicleBox.module.css";
-import { useDataFetcher } from "../../hooks/useDataFetcher";
+// import { useDataFetcher } from "../../hooks/useDataFetcher";
 
 function VehicleBox(props) {
-  const [mainImage, mainImageIsAvailable] = useDataFetcher(
-    `http://localhost:3000/api/img/${props.vehicle.vehicleImages[0]}`
-  );
+  // const [mainImage, mainImageIsAvailable] = useDataFetcher(
+  //   `http://localhost:3000/api/img/${props.vehicle.vehicleImages[0]}`
+  // );
+  // console.log(props.vehicle.vehicleImages[0]);
   return (
     <Link href={`/hangar/vId`} passHref>
       <a>
         <div className={styles.container}>
-          <h2 className={`${styles.title} rebel-font`}>Cube Ams 100 TSDZ2</h2>
+          <h2 className={`${styles.title} rebel-font`}>
+            {props.vehicle.projectName}
+          </h2>
           <div className={styles.categoryImg}>
-            {mainImageIsAvailable ? (
-              <img
-                src={`https://ik.imagekit.io/rebelelectric/tr:w-400${mainImage[0].filePath}`}
-                // src="d"
-                alt="Cube Ams 100 TSDZ2"
-                loading="lazy"
-              />
-            ) : (
+            {/* {mainImageIsAvailable ? ( */}
+            <img
+              src={`https://ik.imagekit.io/rebelelectric/tr:n-big_thumb/hangar/${props.vehicle.vehicleImages[0]}`}
+              // src="d"
+              alt="Cube Ams 100 TSDZ2"
+              loading="lazy"
+            />
+            {/* ) : (
               <img
                 src="/img/rebel-electric-logo.svg"
                 alt="Rebel vehicle"
                 loading="lazy"
                 style={{ background: "#acb0bd", objectFit: "contain" }}
               />
-            )}
+            )} */}
           </div>
 
           <DataBar
