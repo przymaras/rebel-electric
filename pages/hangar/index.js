a814805c01900f6472bde34251aac2e3228a3abe
przymaras <przymaras@gmail.com>
2022-01-19 22:53:39 +0100

    Add static props to hangar page

--- pages/hangar/index.js
+++ pages/hangar/index.js
@@ -1,25 +1,67 @@
+import { MongoClient } from "mongodb";
 import Hangar from "../../components/hangar/Hangar";
 
 function HangarPage(props) {
-  return <Hangar hangarData={props.hangarData} />;
+  return <Hangar vehicles={props.vehicles} />;
 }
 
-export async function getServerSideProps() {
-  let hangarData = { vehicles: [] };
-  const url = `${process.env.API_URL}/vehicles/`;
+// export async function getServerSideProps() {
+//   let hangarData = { vehicles: [] };
+//   const url = `${process.env.API_URL}/vehicles/`;
+//   try {
+//     const res = await fetch(url);
+//     const data = await res.json();
+//     console.log(res.ok);
+//     hangarData = { ...data };
+//   } catch (err) {
+//     console.log(err);
+//   }
+
+//   return {
+//     props: {
+//       hangarData,
+//     },
+//   };
+// }
+
+export async function getStaticProps() {
+  const dbName = process.env.MONGODB_DB;
+  const dbhost = process.env.MONGODB_HOST;
+  const dbUser = process.env.MONGODB_USER;
+  const dbPass = process.env.MONGODB_PASS;
+  const connectString = `mongodb+srv://${dbUser}:${dbPass}@${dbhost}/${dbName}?retryWrites=true&w=majority`;
+
+  let vehicles = { vehicles: [] };
+
   try {
-    const res = await fetch(url);
-    const data = await res.json();
-    console.log(res.ok);
-    hangarData = { ...data };
+    const client = await MongoClient.connect(connectString);
+    const db = client.db();
+    const vehiclesCollection = db.collection("vehicles");
+    vehicles = await vehiclesCollection
+      .aggregate([
+        {
+          $project: {
+            ownerId: 0,
+          },
+        },
+      ])
+
+      .sort({ createdAt: -1 }) //sort from newest to oldest
+      // .limit(3)
+      .toArray();
+    client.close();
   } catch (err) {
     console.log(err);
   }
 
   return {
     props: {
-      hangarData,
+      vehicles: vehicles.map((vehicle) => ({
+        ...vehicle,
+        _id: vehicle._id.toString(),
+      })),
     },
+    revalidate: 20,
   };
 }
 
