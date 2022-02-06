9a61ca769f7f5391e92a5077f2aef84d5084b860
przymaras <przymaras@gmail.com>
2022-02-06 14:46:45 +0100

    Read category in VehicleDetails

--- src/utils/common-functions.ts
+++ src/utils/common-functions.ts
@@ -1,3 +1,5 @@
+import { VehiclesCategories } from "../models/hangar";
+
 export const dataOrOther = (
   value: string | undefined,
   valueOther: string | undefined,
@@ -47,4 +49,50 @@ export const getFullSrc: ImgProjNameFn = (imgName, projName) => {
   const [imageName, imageExtension] = imgName.split(".");
   const underscoredProjectName = projName.split(" ").join("_");
   return `https://ik.imagekit.io/rebelelectric/ik-seo/tr:pr-true,di-rebel.jpg/hangar/${imageName}/${underscoredProjectName}.${imageExtension}`;
+};
+
+export const getSelectedCategoriesNamesAndImages = (
+  cat: VehiclesCategories,
+  selected: number[] | string[]
+) => {
+  let categoriesNames: string[][] = [];
+  let currentIndex = -1;
+
+  const getNames: (
+    cat: VehiclesCategories,
+    selected: number[] | string[]
+  ) => void = (cat, selected) => {
+    currentIndex++;
+    if (
+      selected[currentIndex] !== undefined &&
+      cat.categories[Number(selected[currentIndex])] !== undefined
+    ) {
+      if (cat.categories[Number(selected[currentIndex])].child) {
+        categoriesNames = [
+          ...categoriesNames,
+          [
+            cat.catTitle,
+            cat.categories[Number(selected[currentIndex])].name,
+            cat.categories[Number(selected[currentIndex])].image,
+          ],
+        ];
+        getNames(
+          cat.categories[Number(selected[currentIndex])].child!,
+          selected
+        );
+      } else {
+        categoriesNames = [
+          ...categoriesNames,
+          [
+            cat.catTitle,
+            cat.categories[Number(selected[currentIndex])].name,
+            cat.categories[Number(selected[currentIndex])].image,
+          ],
+        ];
+      }
+    }
+  };
+
+  getNames(cat, selected);
+  return categoriesNames;
 };
