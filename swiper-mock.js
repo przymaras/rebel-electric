c764a832ce7dbbe8c97c01c5d74f9fc636a525f0
przymaras <przymaras@gmail.com>
2022-05-07 13:11:44 +0200

    Install Jest

---
+++ swiper-mock.js
@@ -0,0 +1,10 @@
+export const Swiper = ({
+  onBeforeInit,
+  slidesPerView,
+  slidesPerGroup,
+  spaceBetween,
+  centeredSlides,
+  centeredSlidesBounds,
+  children,
+}) => <div>{children}</div>;
+export const SwiperSlide = 'div';
