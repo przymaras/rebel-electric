7f3431bf60374e73b56b9a4dd9afdf9d622cbe3a
przymaras <przymaras@gmail.com>
2022-07-31 11:54:10 +0200

    Update i18n routing middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,15 +2,26 @@ import { NextRequest, NextResponse } from 'next/server';
 
 const PUBLIC_FILE = /\.(.*)$/;
 
-export function middleware(request: NextRequest) {
-  const shouldHandleLocale =
-    !PUBLIC_FILE.test(request.nextUrl.pathname) &&
-    !request.nextUrl.pathname.includes('/api/') &&
-    request.nextUrl.locale === 'default';
+export function middleware(req: NextRequest) {
+  // const shouldHandleLocale =
+  //   !PUBLIC_FILE.test(request.nextUrl.pathname) &&
+  //   !request.nextUrl.pathname.includes('/api/') &&
+  //   request.nextUrl.locale === 'default';
 
-  const url = process.env.SITE_URL;
+  // const url = process.env.SITE_URL;
 
-  if (!url) return undefined;
+  // if (!url) return undefined;
 
-  return shouldHandleLocale ? NextResponse.redirect(`${url}/en${request.nextUrl.href}`) : undefined;
+  // return shouldHandleLocale ? NextResponse.redirect(`${url}/en${request.nextUrl.href}`) : undefined;
+  if (
+    req.nextUrl.pathname.startsWith('/_next') ||
+    req.nextUrl.pathname.includes('/api/') ||
+    PUBLIC_FILE.test(req.nextUrl.pathname)
+  ) {
+    return;
+  }
+
+  if (req.nextUrl.locale === 'default') {
+    return NextResponse.redirect(new URL(`/en${req.nextUrl.pathname}`, req.url));
+  }
 }
